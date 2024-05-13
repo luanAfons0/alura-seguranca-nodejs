5edf374cfdc25266cde70eaec69ef8c77834228b
luanAfons0 <luan.hmatao@gmail.com>
2024-05-12 21:13:17 -0300

    feat: implemented user routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,10 +1,8 @@
-const bodyParser = require('body-parser')
- 
-const produto = require('./produtoRoute')
+const bodyParser = require("body-parser");
 
-module.exports = app => {
-  app.use(
-    bodyParser.json(),
-    produto
-  )
-}
+const produto = require("./produtoRoute");
+const usuarios = require("./usuariosRoutes");
+
+module.exports = (app) => {
+  app.use(bodyParser.json(), produto, usuarios);
+};
