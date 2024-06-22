c03778ac17996b570337dbb46b18380e84602400
luanAfons0 <luan.hmatao@gmail.com>
2024-06-22 11:51:50 -0300

    fix: finished db conflicts

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,20 +1,7 @@
 const bodyParser = require("body-parser");
 
 const produto = require("./produtoRoute");
-const usuario = require("./usuariosRoute");
-const auth = require("./authRoute");
-const role = require("./role");
-const permissao = require("./permissao");
-const seguranca = require("./seguranca");
 
 module.exports = (app) => {
-  app.use(
-    bodyParser.json(),
-    auth,
-    usuario,
-    produto,
-    role,
-    permissao,
-    seguranca
-  );
+  app.use(bodyParser.json(), produto);
 };
--- api/routes/seguranca.js
+++
@@ -1,8 +0,0 @@
-const Router = require("express");
-const SegurancaController = require("../controllers/segurancaController");
-
-const router = Router();
-
-router.post("/seguranca/acl", SegurancaController.cadastrarACL);
-
-module.exports = router;
